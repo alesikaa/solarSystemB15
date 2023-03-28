366fde36298daa302f8bdba2c167160fada6af91
alesikaa <oleseacucerean@gmail.com>
2023-03-28 19:18:34 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        solarSystem solar=new solarSystem();
+        solar.sun="2";
+        solar.moon="2";
+        solar.planets="2";
+
+
+    }
+}
