c6b12b84c0bbe2a490f366c2317ef73b9c7dab53
alesikaa <oleseacucerean@gmail.com>
2023-03-28 20:25:48 -0400

    I added the feature sun but there are errors that need to be resolved

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         solarSystem solar=new solarSystem();
-        solar.sun="2";
+        solar.sun="1";
         solar.moon="2";
         solar.planets="2";
 
